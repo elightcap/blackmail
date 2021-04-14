62b451c26e23ae52210a3221907e0f98a878f540
elightcap <evane@elightcap.com>
2021-04-13 17:03:54 -0700

    complete refactor

--- blackmailbot.py
+++ blackmailbot.py
@@ -1,36 +1,26 @@
-# bot.py
 import os
-import requests
 import discord
-import threading
-import time
-from twister.internet import task, reactor
-
+from discord.ext import tasks, commands
 from dotenv import load_dotenv
-from operator import itemgetter
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-
 client = discord.Client()
-timeout = 7200.0
-bot = commands.Bot(command_prefix='.'
-@bot.event
-async def on_ready():
-     removeRole.start()
-     print('bot is active')
+Blackmail_role_name = "Blackmailer"
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
 
-@tasks.loop(seconds=10)
-async def removeRole():
-     channel = bot.getchannel(setup)
-     remove = "!mass-role remove @everyone @blackmailer"
-     send = await message.channel.send(remove)
-     time.sleep(5)
-     confirm = "yes"
-     send2 = await message.channel.send(confirm)
-     time.sleep(5)
-     await send.delete()
-     await send2.delete()
+@tasks.loop(seconds=5)
+async def get_all_members_ids(discguild):
+     for guild in client.guilds:
+          for member in guild.members:
+               for role in member.roles:
+                    if role.name == "Blackmailer":
+                         role = discord.utils.get(member.guild.roles, name="Blackmailer")
+                         await member.remove_roles(role)
 
-bot.run(token)
+get_all_members_ids.start(GUILD)
+client.run(TOKEN)
+
