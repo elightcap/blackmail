e0ed880f24a29cdfa3ae52705f546ddd3217aba5
Evan Lightcap <evan@elightcap.com>
2021-04-23 12:47:48 -0400

    subntract money ionstead of add

--- blackmailbot.py
+++ blackmailbot.py
@@ -51,7 +51,8 @@ async def on_message(message):
          else:
               mes = "!pac {0.author.mention} {1}".format(message, amount)
               send = await message.channel.send(mes)
-              builder = {'cash': cost}
+              nCost = '-'+ cost
+              builder = {'cash': nCost}
               jsonString = json.dumps(builder, indent=4)
               rp = requests.patch(url, headers=headers, data=jsonString)
               print(jsonString)
