21b26628376bf611acbc05e0d510c81d6ec807cf
Evan Lightcap <evan@elightcap.com>
2021-04-23 15:28:59 -0400

    idk formatting

--- blackmailbot.py
+++ blackmailbot.py
@@ -23,74 +23,64 @@ headers = {'Authorization': unbkey}
 async def get_all_members_ids(discguild):
      for guild in client.guilds:
           for member in guild.members:
-               for role in member.roles:
+            for role in member.roles:
                     if role.name == "Blackmailer":
                          role = discord.utils.get(
-                             member.guild.roles, name="Blackmailer")
+                              member.guild.roles, name="Blackmailer")
                          await member.remove_roles(role)
 
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+     if message.author == client.user:
         return
-    msg = message.content
-    case = msg.lower()
-    if "!get chips" in case:
-         aID = message.author.id
-         sAmount = case.replace("!get chips ", "")
-         amount = int(sAmount)
-         url = "https://unbelievaboat.com/api/v1/guilds/86565008669958144/users/{}".format(
-             aID)
-         r = requests.get(url, headers=headers)
-         json_data = json.loads(r.text)
-         print(json_data)
-         strMoney = json_data['cash']
-         uMoney = int(strMoney)
-         cost = amount//3
-         if uMoney < cost:
-              mes = "Not even money. Loser."
-              send = await message.channel.send(mes)
-              return
-         else:
-              mes = "!pac {0.author.mention} {1}".format(message, amount)
-              send = await message.channel.send(mes)
-              nCost = '-' + str(cost)
-              builder = {'cash': nCost}
-              jsonString = json.dumps(builder, indent=4)
-              rp = requests.patch(url, headers=headers, data=jsonString)
-              print(jsonString)
+     msg = message.content
+     case = msg.lower()
+     if "!get chips" in case:
+          aID = message.author.id
+          sAmount = case.replace("!get chips ", "")
+          amount = int(sAmount)
+          url = "https://unbelievaboat.com/api/v1/guilds/86565008669958144/users/{}".format(aID)
+          r = requests.get(url, headers=headers)
+          json_data = json.loads(r.text)
+          print(json_data)
+          strMoney = json_data['cash']
+          uMoney = int(strMoney)
+          cost = amount//3
+               if uMoney < cost:
+                    mes = "Not even money. Loser."
+                    send = await message.channel.send(mes)
+                    return
+               else:
+                    mes = "!pac {0.author.mention} {1}".format(message, amount)
+                    send = await message.channel.send(mes)
+                    nCost = '-' + str(cost)
+                    builder = {'cash': nCost}
+                    jsonString = json.dumps(builder, indent=4)
+                    rp = requests.patch(url, headers=headers, data=jsonString)
+                    print(jsonString) 
+     if "!exchange chips" in case:
+          aID = message.author.id
+          sAmount = case.replace("!exchange chips ", "")
+          amount = int(sAmount)
+          url = "https://unbelievaboat.com/api/v1/guilds/86565008669958144/users/{}".format(aID)
+          mes = "!prc {}".format(sAmount)
+          send = await message.channel.send(mes)
 
-    if "!exchange chips" in case:
-         aID = message.author.id
-         sAmount = case.replace("!exchange chips ", "")
-         amount = int(sAmount)
-         url = "https://unbelievaboat.com/api/v1/guilds/86565008669958144/users/{}".format(
-             aID)
-         mes = "!prc {}".format(sAmount)
-         send = await message.channel.send(mes)
+          def check(user):
+               return user == pokerBotID
 
-         def check(user):
-              return user == pokerBotID
-
-          message = await client.wait_for('message',check=check)
+          message = await client.wait_for('message', check=check)
           if "Done!" in message.content:
                payout = str(amount*3)
                builder = {'cash': payout}
                jsonString = json.dumps(builder, indent=4)
-               rp = requests.patch(url, headers=headers, data = )
+               rp = requests.patch(url, headers=headers, data=)
 
           else:
                mes = "ya broke"
                await message.channel.send(mes)
-          
-
-         
-
-
-
 
 
 get_all_members_ids.start(GUILD)
 client.run(TOKEN)
-
