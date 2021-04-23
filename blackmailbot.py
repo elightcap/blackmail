e08868785d06ef71a1d3b2068edc4204c82e7527
Evan Lightcap <evan@elightcap.com>
2021-04-23 10:07:36 -0400

    first steps for poker

--- blackmailbot.py
+++ blackmailbot.py
@@ -1,16 +1,20 @@
 import os
+import requests
 import discord
+import json
 from discord.ext import tasks, commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+unbkey = os.getenv('UNBKEY')
 client = discord.Client()
 Blackmail_role_name = "Blackmailer"
 intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents=intents)
+headers = {'Authorization': unbkey}
 
 @tasks.loop(seconds=5)
 async def get_all_members_ids(discguild):
@@ -20,6 +24,24 @@ async def get_all_members_ids(discguild):
                     if role.name == "Blackmailer":
                          role = discord.utils.get(member.guild.roles, name="Blackmailer")
                          await member.remove_roles(role)
+
+
+@client.event
+async def on_message(message)
+    if message.author == client.user:
+        return
+    msg = message.content
+    case = msg.lower()
+    if "!buy chips" in case:
+         aID = message.author.id
+         amount = case.replace("!price ", "")
+         url = "https://unbelievaboat.com/api/v1/guilds/86565008669958144/users/{}".format(aID)
+         r = requests.get(url, headers=headers)
+         json_data = json.loads{r.text}
+         uMoney = json_data['cash']
+
+
+
 
 get_all_members_ids.start(GUILD)
 client.run(TOKEN)
