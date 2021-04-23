cc9ff07539d087581aa71c5ccd90353dfff64fb4
Evan Lightcap <evan@elightcap.com>
2021-04-23 17:38:46 -0400

    trying something different

--- blackmailbot.py
+++ blackmailbot.py
@@ -11,6 +11,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 unbkey = os.getenv('UNBKEY')
 pokerBotID = "@Poker Now Bot#8842"
+botUrl = "https://unbelievaboat.com/api/v1/guilds/86565008669958144/users/botid"
 client = discord.Client()
 Blackmail_role_name = "Blackmailer"
 intents = discord.Intents.default()
@@ -37,9 +38,9 @@ async def on_message(message):
      #   return
      msg = message.content
      case = msg.lower()
-     if "!get chips" in case:
+     if "!buyin" in case:
           aID = message.author.id
-          sAmount = case.replace("!get chips ", "")
+          sAmount = case.replace("!buyin ", "")
           amount = int(sAmount)
           url = "https://unbelievaboat.com/api/v1/guilds/86565008669958144/users/{}".format(aID)
           r = requests.get(url, headers=headers)
@@ -61,32 +62,45 @@ async def on_message(message):
                rp = requests.patch(url, headers=headers, data=jsonString)
                print(jsonString)
 
-     if "!exchange chips" in case:
+     if "!cashout" in case:
           aID = message.author.id
-          sAmount = case.replace("!exchange chips ", "")
+          sAmount = case.replace("!cashout ", "")
           amount = int(sAmount)
           url = "https://unbelievaboat.com/api/v1/guilds/86565008669958144/users/{}".format(aID)
           mes = "!prc {0.author.mention} {1}".format(message, sAmount)
           send = await message.channel.send(mes)
           print(message.author)
 
-          @client.event
-          async def on_response(message):
-               def check(m):
-                    return "done!" in m.content and m.channel == channel
+          #@client.event
+          #async def on_message(message):
+          #     print(message)
+          #     def check(m):
+          #          return message.mentions[0] == pokerBotID
+          #
+          #     message = await client.wait_for('message', check=check)
+          #     if "done!" in message.content:
+          #          print("user has chips")
+          #          payout = str(amount*3)
+          #          builder = {'cash': payout}
+          #          jsonString = json.dumps(builder, indent=4)
+          #          rp = requests.patch(url, headers=headers, data=jsonString)
+          #
+          #     else:
+          #          print("user doesnt have chips")
+          #          mes = "ya broke"
+          #          await message.channel.send(mes)
 
-               message = await client.wait_for('message', check=check)
-               if "done!" in message.content:
-                    print("user has chips")
-                    payout = str(amount*3)
-                    builder = {'cash': payout}
-                    jsonString = json.dumps(builder, indent=4)
-                    rp = requests.patch(url, headers=headers, data=jsonString)
-
-               else:
-                    print("user doesnt have chips")
-                    mes = "ya broke"
-                    await message.channel.send(mes)
+     if message.id.author == pokerBotID and "done!" in case:
+          print("users got the chips")
+          payout = str((amount//3)*.9)
+          rake = str((amount//3)*.1)
+          payBuilder = {'cash': payout}
+          rakeBuilder = {'bank': rake}
+          payJson = json.dumps(payBuilder, indent=4)
+          rakeJson = json.dumps(rakeBuilder, indent=4)
+          rp = requests.patch(url, headers=headers, data=payJson)
+          rr = requests.patch(botUrl, headers=headers, data=rakeJson)
+          
 
 
 get_all_members_ids.start(GUILD)
