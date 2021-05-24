fbdcd18e01814a244dadf801a01fad179c4d6975
elightcap <50679504+elightcap@users.noreply.github.com>
2021-05-23 23:08:18 -0400

    string maybe?

--- blackmailbot.py
+++ blackmailbot.py
@@ -185,7 +185,7 @@ async def on_message(message):
           url = "https://unbelievaboat.com/api/v1/guilds/86565008669958144/users/"
           r = requests.get(url, headers=headers)
           json_data = json.loads(r.text)
-          leader = next(item for item in json_data if item["rank"] == 1)
+          leader = next((item for item in json_data if item["rank"] == "1"), None)
           print(leader)
 
 
