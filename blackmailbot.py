c117f7a20adfc7099f0cc9dedfe561d202b40cfc
elightcap <50679504+elightcap@users.noreply.github.com>
2021-05-24 23:57:49 -0400

    change channel

--- blackmailbot.py
+++ blackmailbot.py
@@ -150,7 +150,8 @@ async def on_message(message):
                          return
                     elif "public defender" in loserroles:
                          mes = "{} was barely able to afford a public defender".format(person.mention)
-                         send = await message.channel.send(mes)
+                         channel = client.get_channel(831400394184851456)
+                         send = await channel.send(mes)
                          pd = discord.utils.get(message.guild.roles, name="Public Defender")
                          await person.remove_roles(pd)
                     else:
@@ -173,6 +174,11 @@ async def on_message(message):
                          send = await message.channel.send(mes)
                     pt = discord.utils.get(message.guild.roles, name="Patent Troll")
                     await message.author.remove_roles(pt)
+     ###function that will remove
+     ###10% of casdh leaders total
+     ###money and distributes it
+     ###evenly to everyone with the merry
+     ###people role
      elif "!robinhood" in case:
           roles = [y.name.lower() for y in message.author.roles]
           if "little john" in roles:
@@ -216,13 +222,8 @@ async def on_message(message):
                          builder = {'cash': share}
                          jsonString = json.dumps(builder, indent=4)
                          rp = requests.patch(url, headers=headers, data=jsonString)
-                         print("{} removed from Little John".format(str(member)))
           else:
                return
-          
-
-
-
 
 get_all_members_ids.start(GUILD)
 client.run(TOKEN)
