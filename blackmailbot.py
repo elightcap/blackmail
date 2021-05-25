76691bff438673f8786828f838b1dc31fe573996
elightcap <50679504+elightcap@users.noreply.github.com>
2021-05-25 14:26:38 -0400

    trying some stuff to limit things

--- blackmailbot.py
+++ blackmailbot.py
@@ -91,7 +91,7 @@ async def on_message(message):
                     send = await message.channel.send(mes)
                else:
                     print("buyin")
-                    check = "!pw {message.author.mention} "
+                    check = "!pw {}.author.mention ".format(message)
                     botChannel = client.get_channel(810662162195284049)
                     checkMes = await botChannel.send(check)
                     @client.event
