9c02f803bd5be09e7008e37643c5090829474c28
elightcap <50679504+elightcap@users.noreply.github.com>
2021-05-31 23:32:44 -0400

    add invite

--- cogs/nuke.py
+++ cogs/nuke.py
@@ -5,7 +5,7 @@ class NukeCog(commands.Cog, name="Nuke"):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command(nam='nuke')
+    @commands.command(name='nuke')
     @commands.guild_only()
     async def do_nuke(self, ctx, *, our_input: str):
         await ctx.send(our_input)
