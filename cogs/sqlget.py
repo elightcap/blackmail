16c9a9030af09848bae1b350380b953abf1a9afc
elightcap <50679504+elightcap@users.noreply.github.com>
2021-06-01 19:16:27 -0400

    fix select

--- cogs/sqlget.py
+++ cogs/sqlget.py
@@ -7,19 +7,19 @@ load_dotenv()
 dbUser = os.getenv('MARIAUSER')
 dbPass = os.getenv('MARIAPASS')
 
-async def sql_select(db, table, uid):
+async def sql_select(db, table, attname, att):
     try:
-            connection = database.connect(
-                    user = dbUser,
-                    password = dbPass,
-                    host='localhost',
-                    db=db
-            )
-            cursor = connection.cursor(buffered=True)
-            statement = """SELECT * FROM {table_name} where owner=(%s)""".format(table_name=table)
-            data = (uid,)
-            cursor.execute(statement,data)
-            rows = cursor.fetchall()
-            return rows
+        connection = database.connect(
+        user = dbUser,
+        password = dbPass,
+        host='localhost',
+        db=db
+        )
+        cursor = connection.cursor(buffered=True)
+        statement = """SELECT * FROM {table_name} where {att_name} like (%s)""".format(table_name=table, att_name=attname)
+        data = (table, att)
+        cursor.execute(statement, data)
+        rows = cursor.fetchall()
+        return rows
     except database.Error as e:
         print(f" remove {e}")
