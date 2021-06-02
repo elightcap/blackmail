c4f0ccb2e7cc5bb7d046f6d90f07c2c06b61c4b0
elightcap <50679504+elightcap@users.noreply.github.com>
2021-06-01 21:22:43 -0400

    rename

--- cogs/db.py
+++ cogs/db.py
@@ -22,7 +22,7 @@ headers = {'Authorization': unbkey}
 pokerBotID = "613156357239078913"
 botUrl = "https://unbelievaboat.com/api/v1/guilds/86565008669958144/users/613156357239078913"
 
-class CommandsCog(commands.Cog, name="Commands"):
+class dbCog(commands.Cog, name="db"):
     def __init__(self, bot):
         self.bot = bot
         self.remove_rv.start()
@@ -64,4 +64,4 @@ class CommandsCog(commands.Cog, name="Commands"):
                   print(e)
 
 def setup(bot):
-    bot.add_cog(CommandsCog(bot))
+    bot.add_cog(dbCog(bot))
