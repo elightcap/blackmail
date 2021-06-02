3996b87d5ec6d85ea2aed5f88ae8445c1f42a9ce
elightcap <50679504+elightcap@users.noreply.github.com>
2021-06-02 15:03:51 -0400

    db role management

--- cogs/db.py
+++ cogs/db.py
@@ -26,6 +26,7 @@ class dbCog(commands.Cog, name="db"):
     def __init__(self, bot):
         self.bot = bot
         self.remove_rv.start()
+        self.remove_lawyer.start()
 
     @tasks.loop(minutes=1)
     async def remove_rv(self):
@@ -58,6 +59,75 @@ class dbCog(commands.Cog, name="db"):
                                                 role  = discord.utils.get(member.guild.roles, name="Robbery Victim")
                                                 if member.id == uUid:
                                                      await sql_remove("robbed", "users", "uid", uUid)
+                                                     print("rv role removed")
+                                                     await member.remove_roles(role)
+             except os.error as e:
+                  print(e)
+
+    @tasks.loop(minutes=1)
+    async def remove_lawyer(self):
+        rows = await sql_select("lawyer", "users", "uid","%")
+        for row in rows:
+             uUid = int(row[0])
+             print(uUid)
+             uDate = row[1]
+             strTime = str(row[2])
+             uTime = datetime.strptime(strTime,"%H:%M:%S" ).time()
+             try:
+                  datetimenow = datetime.now()
+                  addTime = timedelta(minutes=1)
+                  addDays = timedelta(days=2)
+                  dateNowStr = datetimenow.strftime("%Y-%m-%d")
+                  timeNowStr = datetimenow.strftime("%H:%M:%S")
+                  mDate = (datetime.strptime(dateNowStr, "%Y-%m-%d")).date()
+                  mTime = datetime.strptime(timeNowStr, "%H:%M:%S")
+                  newTime = (mTime - addTime).time()
+                  newDate = (mDate - addDays)
+                  if uDate <= mDate:
+                       print("date")
+                       if uTime <= newTime:
+                            print("time")
+                            member = self.bot.get_user(uUid)
+                            print(member)
+                            for guild in self.bot.guilds:
+                                 for member in guild.members:
+                                      for role in member.roles:
+                                           if role.name == "Lawyer'd Up":
+                                                print("role")
+                                                role  = discord.utils.get(member.guild.roles, name="Lawyer'd Up")
+                                                if member.id == uUid:
+                                                     await sql_remove("lawyer", "users", "uid", uUid)
+                                                     print("lawyer role removed")
+                                                     await member.remove_roles(role)
+             except os.error as e:
+                  print(e)
+
+    @tasks.loop(minutes=1)
+    async def remove_blackmail(self):
+        rows = await sql_select("blackmailer", "users", "uid","%")
+        for row in rows:
+             uUid = int(row[0])
+             uDate = row[1]
+             strTime = str(row[2])
+             uTime = datetime.strptime(strTime,"%H:%M:%S" ).time()
+             try:
+                  datetimenow = datetime.now()
+                  addTime = timedelta(hours=2)
+                  dateNowStr = datetimenow.strftime("%Y-%m-%d")
+                  timeNowStr = datetimenow.strftime("%H:%M:%S")
+                  mDate = (datetime.strptime(dateNowStr, "%Y-%m-%d")).date()
+                  mTime = datetime.strptime(timeNowStr, "%H:%M:%S")
+                  newTime = (mTime - addTime).time()
+                  if uDate <= mDate:
+                       if uTime <= newTime:
+                            member = self.bot.get_user(uUid)
+                            for guild in self.bot.guilds:
+                                 for member in guild.members:
+                                      for role in member.roles:
+                                           if role.name == "Blackmailer":
+                                                role  = discord.utils.get(member.guild.roles, name="Blackmailer")
+                                                if member.id == uUid:
+                                                     await sql_remove("blackmailer", "users", "uid", uUid)
                                                      print("blackmail role removed")
                                                      await member.remove_roles(role)
              except os.error as e:
