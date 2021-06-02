fb215d3bcd77f9afe98f92f3ec65e1919c56dd5f
elightcap <50679504+elightcap@users.noreply.github.com>
2021-06-02 15:27:53 -0400

    testing user add roles

---
+++ cogs/dbadd.py
@@ -0,0 +1,32 @@
+import discord
+import os
+from typing import cast
+from discord.ext import commands,tasks
+from discord.utils import get
+from dotenv import load_dotenv
+from cogs.sqlget import sql_select
+from cogs.sqladd import sql_insert
+from datetime import datetime, timedelta
+
+load_dotenv()
+intents = discord.Intents.default()
+intents.members = True
+intents.messages=True
+client = discord.Client(intents=intents)
+unbkey = os.getenv('UNBKEY')
+headers = {'Authorization': unbkey}
+pokerBotID = "613156357239078913"
+botUrl = "https://unbelievaboat.com/api/v1/guilds/86565008669958144/users/613156357239078913"
+
+class dbCog(commands.Cog, name="db"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @client.event
+    async def on_member_update(before,after):
+        if len(before.roles)<len(after.roles):
+            newRole = next(role for role in after.roles if role not in before.roles)   
+            if newRole.name == "Robbery Victim":
+                uid = int(after.id)
+                await sql_insert("robbed", "users", uid)
+                print("rob vic success add")
