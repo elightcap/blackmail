aa5d33a967ead62edd740e03945c704196d845a7
elightcap <50679504+elightcap@users.noreply.github.com>
2021-06-02 15:33:59 -0400

    comma

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ def get_prefix(bot, message):
     return commands.when_mentioned_or(*prefixes)(bot, message)
 
 initial_extensions = ['cogs.commands',
-                                  'cogs.db'
+                                  'cogs.db',
                                   'cogs.dbadd'
                                 ]
 
