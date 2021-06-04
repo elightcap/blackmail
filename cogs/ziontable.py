af6d743a27979e61d63ccb3afed3405211ffb598
elightcap <evane@elightcap.com>
2021-06-03 21:06:37 -0700

    got channel updates and shit

--- cogs/ziontable.py
+++ cogs/ziontable.py
@@ -21,8 +21,8 @@ class channelTableCog(commands.Cog, name="channelTable"):
 
     @tasks.loop(minutes=1)
     async def update_table(self):
-        msg = self.bot.fetch_message(850221111819370517)
-        embedVar = discord.Embed(title="Channels", description="The channels", color=0x00ff00)
+        await self.bot.wait_until_ready()
+        embedVar = discord.Embed(title="Channels Fuck You", description="The channels", color=0x00ff00)
         rows = await sql_select("channels", "owners", "owner", "%")
         if rows:
             for row in rows:
@@ -32,7 +32,7 @@ class channelTableCog(commands.Cog, name="channelTable"):
                 oChannel = self.bot.get_channel(cID)
                 embedVar.add_field(name=f'**{oChannel.name}**', value=f'> Owned By: {owner.name}' )
         channel = self.bot.get_channel(850206859898257408)
-        msg = channel.fetch_message(850221111819370517)
+        msg = await channel.fetch_message(850221103963045898)
         send = await msg.edit(embed=embedVar)
 
 
