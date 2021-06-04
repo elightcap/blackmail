edf81e93e5da53de2ad765ce727508c1049d39cf
elightcap <50679504+elightcap@users.noreply.github.com>
2021-06-03 23:57:56 -0400

    trying to edit message

--- cogs/ziontable.py
+++ cogs/ziontable.py
@@ -21,23 +21,18 @@ class channelTableCog(commands.Cog, name="channelTable"):
 
     @tasks.loop(minutes=1)
     async def update_table(self):
-        await self.bot.wait_until_ready()
+        msg = self.bot.fetch_message(850221111819370517)
         embedVar = discord.Embed(title="Channels", description="The channels", color=0x00ff00)
         rows = await sql_select("channels", "owners", "owner", "%")
         if rows:
             for row in rows:
-                print(row)
                 oID = int(row[0])
                 cID = int(row[1])
-                print(oID)
-                print(cID)
                 owner = self.bot.get_user(oID)
                 oChannel = self.bot.get_channel(cID)
-                print(oChannel)
-                print(owner)
                 embedVar.add_field(name=f'**{oChannel.name}**', value=f'> Owned By: {owner.name}' )
         channel = self.bot.get_channel(850206859898257408)
-        send = await channel.send(embed=embedVar)
+        send = await msg.edit(embed=embedVar)
 
 
 def setup(bot):
