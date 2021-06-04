773a879be5764096a8c23f8816c85243357b86ad
elightcap <50679504+elightcap@users.noreply.github.com>
2021-06-04 00:16:06 -0400

    testing update embed on channel update

--- cogs/channelupdate.py
+++ cogs/channelupdate.py
@@ -24,8 +24,19 @@ class channelUpdateCog(commands.Cog, name="channelUpdate"):
 
     @commands.Cog.listener()
     async def on_guild_channel_update(self, before, after):
-        print(before.name)
-        print(after.name)
+        rows = sql_select("channels","owners","owner","%")
+        embedVar = discord.Embed(title="Channels Fuck You", description="The channels", color=0x00ff00)
+        rows = await sql_select("channels", "owners", "owner", "%")
+        if rows:
+            for row in rows:
+                oID = int(row[0])
+                cID = int(row[1])
+                owner = self.bot.get_user(oID)
+                oChannel = self.bot.get_channel(cID)
+                embedVar.add_field(name=f'**{oChannel.name}**', value=f'> Owned By: {owner.name}' )
+        channel = self.bot.get_channel(850206859898257408)
+        msg = await channel.fetch_message(850221103963045898)
+        send = await msg.edit(embed=embedVar)
 
 def setup(bot):
     bot.add_cog(channelUpdateCog(bot))
